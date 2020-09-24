67ec20e4709a434d2a1d81d48a88f709cc69ec87
FrankR-7 <franklinrosales7@gmail.com>
2020-09-23 18:52:48 -0600

    Still not working

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from flask_cors import CORS, cross-origin
+from flask_cors import CORS, cross_origin
 import json
 
 app = Flask(__name__)
@@ -7,6 +7,7 @@ CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 def hello_world():
   with open("projects.json") as stream:
     data = json.load(stream)

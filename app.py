9ad9b5265f25385c2bca4483027db7ae8e244f37
FrankR-7 <franklinrosales7@gmail.com>
2020-09-23 18:47:51 -0600

    Copy pasting stuff

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross-origin
 import json
 
 app = Flask(__name__)
 CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def hello_world():
